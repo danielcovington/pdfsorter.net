af88c1272d535e4b828f76f4714106326fb89ab1
Kyle Chamberlin <KyleC@KentCommunications.com>
2014-09-17 08:48:35 -0400

    Single passing test

---
+++ pdfsorter.net/Pdf.vb
@@ -0,0 +1,14 @@
+﻿Imports Microsoft.VisualBasic
+
+Public Class Pdf
+    Private numberOfPages As Integer
+
+    Public Sub New()
+        Me.numberOfPages = 0
+    End Sub
+
+    Public Function getNumberOfPages() As Integer
+        Return Me.numberOfPages
+    End Function
+
+End Class
---
+++ pdfsorter.net/pdfTest.vb
@@ -0,0 +1,13 @@
+﻿Imports System.Text
+Imports Microsoft.VisualStudio.TestTools.UnitTesting
+Imports pdfsorter.net.Pdf
+
+<TestClass()> Public Class PdfTest
+
+    <TestMethod()> Public Sub NewPdfHasZeroPagesWhenInitializedTest()
+        Dim testPdf As Pdf = New Pdf()
+        Assert.AreEqual(testPdf.getNumberOfPages(), 0)
+
+    End Sub
+
+End Class
